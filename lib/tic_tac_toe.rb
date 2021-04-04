4e300ad9d175ab9506e6cfcb156eabefcc7a96bf
Erica Little <eteney90@gmail.com>
2021-04-04 18:49:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -81,4 +81,20 @@ class TicTacToe
      won? || full? || draw?
     end
 
+    def winner
+      if won?
+        @board[won?[0]]
+      end
+    end
+
+    def play
+      while !over? && turn_count<9
+        turn
+      end
+      if won?
+        puts "Congratulations #{winner}!"
+      elsif draw?
+        puts "Cat's Game!"
+      end
+    end
 end
