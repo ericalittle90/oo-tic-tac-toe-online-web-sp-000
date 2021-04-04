60659d61e4a58ceb7ce15973969d1bd315bd8e9d
Erica Little <eteney90@gmail.com>
2021-04-04 18:34:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,6 +49,16 @@ class TicTacToe
     turn_count%2==0 ? "X" : "O"
   end
 
+  def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else turn(board)
+    end
+  end
 
 
 
