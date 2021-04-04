45db676442557c3ecf73aa017d7b50116461e243
Erica Little <eteney90@gmail.com>
2021-04-04 18:09:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 class TicTacToe
 
-  def intialize(board = nil) 
-    @board = board || Array.new(9, " ") 
-  end 
+  def intialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
 
 end 
