9ec7450f8f78353345c82f72f79a857fad1b8af0
Erica Little <eteney90@gmail.com>
2021-04-04 18:33:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,11 +38,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    if index.between?(0,8) && !position_taken?(index)
-      true
-    else
-      false
-    end
+    index.between?(0,8) && !position_taken?(index)
   end
 
   def turn_count
